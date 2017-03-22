84a894bf113cb0389f559e373b259e3e2089bfec
Dong <dongtm@vccloud.vn>
2017-03-22 10:52:09 +0700

    new code

--- 1.js
+++ 1.js
@@ -1,4 +1,19 @@
 ﻿ $(function(){
- 
+ $('#carousel-example-generic').cslider({
+
+	current		: 0, 	
+	// index of current slide
+	
+	bgincrement	: 50,	
+	// increment the background position 
+	// (parallax effect) when sliding
+	
+	autoplay	: true,
+	// slideshow on / off
+	
+	interval	: 4000  
+	// time between transitions
+	
+});
 })  
  
